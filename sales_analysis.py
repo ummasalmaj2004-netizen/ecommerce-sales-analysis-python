6bf5713bf1c77783e0f0e4c428e7b53f6c660f7e
Ummasalma Jamil <ummasalmaj2004@gmail.com>
2026-08-15 00:13:34 +0800

    Add revenue calculation for smartphone and comparison

--- sales_analysis.py
+++ sales_analysis.py
@@ -7,3 +7,24 @@ price = 3500
 revenue = quantity * price
 
 print("Revenue:", revenue)
+product2 = "Smartphone"
+quantity2 = 5
+price2 = 1800
+
+revenue2 = quantity2 * price2
+
+print("Smartphone Revenue:", revenue2)
+total_revenue = revenue + revenue2
+
+print("Total Revenue:", total_revenue)
+if revenue > revenue2:
+    print("Laptop made more revenue")
+else:
+    print("Smartphone made more revenue")
+
+
+
+
+
+
+
